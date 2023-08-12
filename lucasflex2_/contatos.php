cb8bde44743044b4ef12bb992932c4c337578b4a
LeonardoF27 <67442454+LeonardoF27@users.noreply.github.com>
2023-08-11 23:53:04 -0300

    Add files via upload

---
+++ lucasflex2_/contatos.php
@@ -0,0 +1,46 @@
+<?php require "inc/cabecalho__produtos.php"; ?>
+
+<main class="contato centraliza">
+
+<h2 class="titulos">Fale Conosco</h2>
+
+<p>Por favor, preencha o formulário abaixo para que possamos entrar em contato o mais breve possível.</p>
+
+<div>
+    <form action="">
+
+<div>
+<div><label for="">Nome:</label></div>
+<div><input type="text" name="" id=""></div>
+</div>
+
+<div>
+<div><label for="">Email:</label></div>
+<div><input type="email" name="" id=""></div>
+</div>
+
+<div>
+<div><label for="">Telefone:</label></div>
+<div><input type="tel" name="" id=""></div>
+</div>
+
+<div>
+<div><label for="">Assunto:</label></div>
+<div><input type="text" name="" id=""></div>
+</div>
+
+<div>
+<div><label for="">Mensagem:</label></div>
+<div><textarea name="" id="" cols="30" rows="10" style="height:150px"></textarea></div>
+</div>
+
+<div>
+    <input type="submit" value="Enviar">
+</div>
+
+    </form>
+</div>
+
+</main>
+
+<?php require "inc/rodape__inicial.php"; ?>
